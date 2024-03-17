93ec27b8dc959c59f4bd6a1bdabc128ed73a9fe8
drkgrkn <dorukgurkan1@gmail.com>
2024-03-17 12:42:25 +0300

    solution of replication:4

--- app/protocol/replication.go
+++ app/protocol/replication.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "github.com/codecrafters-io/redis-starter-go/app/utils"
+
 type Role string
 
 const (
@@ -8,7 +10,9 @@ const (
 )
 
 type ReplicationState struct {
-	role Role
+	role             Role
+	masterRepliID    *string
+	masterReplOffset *int
 }
 
 type ReplicationStateOptFunc func(*ReplicationState)
@@ -16,14 +20,20 @@ type ReplicationStateOptFunc func(*ReplicationState)
 func ReplicaOf(address string, port int) ReplicationStateOptFunc {
 	return func(rs *ReplicationState) {
 		rs.role = Slave
+		rs.masterRepliID = nil
+		rs.masterReplOffset = nil
 	}
 }
 
 var replicationState ReplicationState
 
 func InitReplicationState(opts []ReplicationStateOptFunc) error {
+	repliID := utils.RandomString(40)
+	repliOffset := 0
 	replicationState = ReplicationState{
-		role: Master,
+		role:             Master,
+		masterRepliID:    &repliID,
+		masterReplOffset: &repliOffset,
 	}
 	for _, f := range opts {
 		f(&replicationState)
@@ -33,4 +43,10 @@ func InitReplicationState(opts []ReplicationStateOptFunc) error {
 
 func (rs ReplicationState) Role() Role {
 	return rs.role
+}
+func (rs ReplicationState) MasterReplicationID() *string {
+	return rs.masterRepliID
+}
+func (rs ReplicationState) MasterReplicationOffset() *int {
+	return rs.masterReplOffset
 }
---
+++ app/utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+var alphaNumeric = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func RandomString(n int) string {
+	b := make([]rune, n)
+	for i := 0; i < n; i++ {
+		b[i] = alphaNumeric[rand.Intn(len(alphaNumeric))]
+	}
+	return string(b)
+}
