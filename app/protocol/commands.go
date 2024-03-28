c179ee0131cdd3653c49856fefb8f0ae73ee74fa
doruk.gurkan <doruk.gurkan@accenture.com>
2024-03-28 13:30:18 +0300

    promoted master lock

--- app/protocol/commands.go
+++ app/protocol/commands.go
@@ -198,7 +198,7 @@ func (s *Server) processWaitRequest(c *Connection, msg Message) error {
 		}
 	}
 	fmt.Printf("%d replicas are currently in sync\n", currInSyncCount)
-	if currInSyncCount >= reqInSyncReplCount {
+	if currInSyncCount >= reqInSyncReplCount || len(s.masterConfig.slaves) == reqInSyncReplCount {
 		_, err = c.WriteString(SerializeInteger(currInSyncCount))
 		if err != nil {
 			return err
